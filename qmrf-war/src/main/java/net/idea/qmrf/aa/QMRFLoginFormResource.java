b6fc1124f39cdca56d6fb9812d76604f34330bcb
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2012-03-12 22:20:51 +0000

    removing OpenSSO AA, replacing with restlet CookieAuthenticator

---
+++ qmrf-war/src/main/java/net/idea/qmrf/aa/QMRFLoginFormResource.java
@@ -0,0 +1,17 @@
+package net.idea.qmrf.aa;
+
+import net.idea.qmrf.client.Resources;
+import net.idea.rest.protocol.QMRF_HTMLBeauty;
+import net.idea.restnet.aa.local.UserLoginFormResource;
+import net.idea.restnet.c.html.HTMLBeauty;
+
+import org.restlet.security.User;
+
+
+public class QMRFLoginFormResource extends UserLoginFormResource<User> {
+	@Override
+	protected HTMLBeauty getHTMLBeauty() {
+		return new QMRF_HTMLBeauty(Resources.login);
+	}
+	
+}
