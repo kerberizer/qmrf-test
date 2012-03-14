8ca75cb96ab6ad57a7f1e3f57c81de629ef39f4f
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2012-03-14 18:54:02 +0000

    Hints for tabs and larger font size

--- qmrf-db/src/main/java/net/idea/rest/protocol/resource/db/ProtocolQueryHTMLReporter.java
+++ qmrf-db/src/main/java/net/idea/rest/protocol/resource/db/ProtocolQueryHTMLReporter.java
@@ -174,22 +174,22 @@ public class ProtocolQueryHTMLReporter extends QMRFHTMLReporter<DBProtocol, IQue
 			"<div class='tabs'>\n",item.getIdentifier(),hidden?"none":""));
 			
 			output.write(String.format("<ul>\n"+
-			"<li><a href='#tabs-1'>Identifier</a></li>"+
-			"<li><a href='#tabs-2'>General</a></li>"+
-			"<li><a href='#tabs-3'>Endpoint</a></li>"+
-			"<li><a href='#tabs-4'>Algorithm</a></li>"+
-			"<li><a href='#tabs-5'>App. domain</a></li>"+
-			"<li><a href='#tabs-6'>Robustness</a></li>"+
-			"<li><a href='#tabs-7'>Predictivity</a></li>"+
-			"<li><a href='#tabs-8'>Interpretation</a></li>"+
-			"<li><a href='#tabs-9'>Misc</a></li>"+
+			"<li><a href='#tabs-1' title='1.QSAR identifier'>Identifier</a></li>"+
+			"<li><a href='#tabs-2' title='2.General information'>General</a></li>"+
+			"<li><a href='#tabs-3' title='3.Defining the endpoint - OECD Principle 1'>Endpoint</a></li>"+
+			"<li><a href='#tabs-4' title='4.Defining the algorithm - OECD Principle 2'>Algorithm</a></li>"+
+			"<li><a href='#tabs-5' title='5.Defining the applicability domain - OECD Principle 3'>App. domain</a></li>"+
+			"<li><a href='#tabs-6' title='6.Internal validation - OECD Principle 4'>Robustness</a></li>"+
+			"<li><a href='#tabs-7' title='7.External validation - OECD Principle 4'>Predictivity</a></li>"+
+			"<li><a href='#tabs-8' title='8.Providing a mechanistic interpretation - OECD Principle 5'>Interpretation</a></li>"+
+			"<li><a href='#tabs-9' title='9.Miscellaneous information'>Misc</a></li>"+
 			//"<li><a href='#tabs-dataset'>Attachments</a></li>"+
 			"<li>%s<span></span></li>"+
 			"</ul>",attachmentURI));
 	
 			qhtml.xml2summary(getDOMSource(item),output);
 		
-			String uploadUI = String.format("<a href='%s%s/%s' target='upload'>%s</a>",
+			String uploadUI = String.format("<a href='%s%s/%s' target='upload' title='Upload training and test datasets and related documents''>%s</a>",
 					uriReporter.getBaseReference(),Resources.editor,item.getIdentifier(),"Add attachment(s)");
 			output.write(String.format("<div id='Attachments'><span class='summary'>N/A<br>%s</span></div>",uploadUI));
 			
