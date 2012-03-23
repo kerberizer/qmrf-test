8416fa8a3dcc9602182ed8bafb63f870f1a4ea29
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2012-03-23 07:10:53 +0000

    now using XSLT templates (thread safe)

--- qmrf-editor/qmrf-core/src/main/java/net/idea/qmrf/converters/QMRF_xml2html.java
+++ qmrf-editor/qmrf-core/src/main/java/net/idea/qmrf/converters/QMRF_xml2html.java
@@ -7,6 +7,7 @@ import java.io.Writer;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -14,7 +15,10 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 public class QMRF_xml2html {
-	protected Transformer transformer;
+	//Templates are thread safe
+	protected static Templates templates=null;
+	protected Transformer transformer=null;
+	
 	public QMRF_xml2html() {
 	
 	}
@@ -24,13 +28,14 @@ public class QMRF_xml2html {
 	protected InputStream getQMRF2SUMMARY_XSL() throws IOException {
 		return getClass().getClassLoader().getResourceAsStream("ambit2/qmrfeditor/qmrf2summary.xsl");
 	}
-	public void xml2summary(Source sourceDocument, Writer html) throws IOException, TransformerException {
+	public synchronized void  xml2summary(Source sourceDocument, Writer html) throws IOException, TransformerException {
 		InputStream xslt = null;
 		try {
-			if (transformer==null) {
+			if (templates==null) {
 				xslt = getQMRF2SUMMARY_XSL();
-				transformer = getTransformer(new StreamSource(xslt));
+				templates = getTemplates(new StreamSource(xslt));
 			}
+			if (transformer==null) transformer = templates.newTransformer();
 			transformer.transform(sourceDocument,  new StreamResult(html));			
 		} finally {
 			try {if (xslt!=null) xslt.close();} catch (Exception x) {}
@@ -38,7 +43,7 @@ public class QMRF_xml2html {
 	}
 	
 	
-	public void xml2html(Source sourceDocument, Writer html) throws IOException, TransformerException {
+	public synchronized void xml2html(Source sourceDocument, Writer html) throws IOException, TransformerException {
 		InputStream xslt = getQMRF2HTML_XSL();
 		try {
 			Transformer transformer = getTransformer(new StreamSource(xslt));
@@ -47,7 +52,8 @@ public class QMRF_xml2html {
 			try {xslt.close();} catch (Exception x) {}
 		}
 	}
-	public void xml2html(Source sourceDocument, OutputStream html) throws IOException, TransformerException {
+
+	public synchronized void xml2html(Source sourceDocument, OutputStream html) throws IOException, TransformerException {
 		InputStream xslt = getQMRF2HTML_XSL();
 		try {
 			Transformer transformer = getTransformer(new StreamSource(xslt));
@@ -57,17 +63,21 @@ public class QMRF_xml2html {
 		}
 	}
 	
-	public void xsltTransform(InputStream xml, InputStream xslt,OutputStream out) throws IOException, TransformerException {
+	public synchronized void  xsltTransform(InputStream xml, InputStream xslt,OutputStream out) throws IOException, TransformerException {
 		Transformer transformer = getTransformer(new StreamSource(xslt));
 		transformer.transform(new StreamSource(xml),  new StreamResult(out));
 	}
 	
-	protected Transformer getTransformer(Source xslt)   throws IOException, TransformerException {
+	protected synchronized Templates getTemplates(Source xslt)   throws IOException, TransformerException {
+		TransformerFactory xfactory = TransformerFactory.newInstance();
+		return xfactory.newTemplates(xslt);
+	}
+
+	protected synchronized Transformer getTransformer(Source xslt)   throws IOException, TransformerException {
 		TransformerFactory xfactory = TransformerFactory.newInstance();
 		Transformer transformer = xfactory.newTransformer(xslt);
 		transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "qmrf.dtd");
 		return transformer;
 	}
-
 
 }
