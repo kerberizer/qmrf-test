2038531af154b1c4d64211100091b2aeee702b65
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2012-04-15 05:48:51 +0000

    Added missing endpoints class

---
+++ qmrf-web/qmrf-db/src/main/java/net/idea/rest/endpoints/EndpointTest.java
@@ -0,0 +1,29 @@
+package net.idea.rest.endpoints;
+
+import ambit2.base.data.Dictionary;
+
+public class EndpointTest extends Dictionary {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5987449761837302780L;
+	protected String code;
+	
+	public EndpointTest(String arg1,String arg2) {
+		super(arg1,arg2);
+	}
+	
+	public EndpointTest(String arg1,String arg2,String arg3) {
+		super(arg1,arg2,arg3);
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	@Override
+	public String toString() {
+		return String.format("%s.%s",getCode(),getName());
+	}
+}
