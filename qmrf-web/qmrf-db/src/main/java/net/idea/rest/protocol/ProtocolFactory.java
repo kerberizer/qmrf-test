3b5590e182ee0e770fa53fa48b36c0a7fda1d1d3
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2012-05-06 13:08:39 +0000

    Throw Bad Request error when the XML file imported is not valid QMRF

--- qmrf-web/qmrf-db/src/main/java/net/idea/rest/protocol/ProtocolFactory.java
+++ qmrf-web/qmrf-db/src/main/java/net/idea/rest/protocol/ProtocolFactory.java
@@ -112,6 +112,8 @@ public class ProtocolFactory {
 				        protocol.setAbstract(fi.getString(utf8));
 				    	QMRFObject qmrf = new QMRFObject();
 						qmrf.read(new StringReader(protocol.getAbstract()));
+						if (qmrf.getChapters().size()<10) 
+							throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST,"Invalid QMRF document!");
 						protocol.setTitle(QMRFConverter.replaceTags(((QMRFSubChapterText)qmrf.getChapters().get(0).getSubchapters().getItem(0)).getText()));
 						//protocol.setIdentifier(QMRFConverter.replaceTags(((QMRFSubChapterText)qmrf.getChapters().get(9).getSubchapters().getItem(0)).getText()));
 						String keywords = QMRFConverter.replaceTags(((QMRFSubChapterText)qmrf.getChapters().get(9).getSubchapters().getItem(2)).getText());
