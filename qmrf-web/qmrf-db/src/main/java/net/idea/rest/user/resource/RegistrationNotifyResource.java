2714cc9ae35f7083d16a72bebdcc72f3d56d0442
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2012-11-09 12:26:39 +0000

    A dedicated notification resource as a registration result URI

--- qmrf-web/qmrf-db/src/main/java/net/idea/rest/user/resource/RegistrationNotifyResource.java
+++ qmrf-web/qmrf-db/src/main/java/net/idea/rest/user/resource/RegistrationNotifyResource.java
@@ -1,23 +1,16 @@
 package net.idea.rest.user.resource;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import net.idea.modbcum.i.exceptions.AmbitException;
-import net.idea.modbcum.i.processors.IProcessor;
 import net.idea.qmrf.client.Resources;
-import net.idea.qmrf.client.Resources.Config;
 import net.idea.rest.QMRFHTMLReporter;
 import net.idea.rest.protocol.QMRF_HTMLBeauty;
-import net.idea.rest.user.CallableUserCreator;
-import net.idea.rest.user.DBUser;
 import net.idea.restnet.c.TaskApplication;
 import net.idea.restnet.c.html.HTMLBeauty;
 import net.idea.restnet.c.resource.CatalogResource;
-import net.idea.restnet.db.DBConnection;
 import net.idea.restnet.i.task.ICallableTask;
 
 import org.restlet.Context;
@@ -31,23 +24,24 @@ import org.restlet.representation.Representation;
 import org.restlet.representation.Variant;
 import org.restlet.resource.ResourceException;
 
-public class RegistrationResource extends CatalogResource<DBUser> {
-	protected List<DBUser> dummyuser = new ArrayList<DBUser>();
-	public RegistrationResource() {
+public class RegistrationNotifyResource  extends CatalogResource<String> {
+	protected List<String> dummyuser = new ArrayList<String>();
+	
+	public RegistrationNotifyResource() {
 		super();
 		setHtmlbyTemplate(true);
-		dummyuser.add(null);
+		dummyuser.add(String.format("%s%s",Resources.register,Resources.notify));
 	}
 	
 	@Override
-	protected Iterator<DBUser> createQuery(Context context, Request request,
+	protected Iterator<String> createQuery(Context context, Request request,
 			Response response) throws ResourceException {
 		return dummyuser.iterator();
 	}
 
 	@Override
 	public String getTemplateName() {
-		return "register.ftl";
+		return "register_notify.ftl";
 	}
 
 	
@@ -74,11 +68,15 @@ public class RegistrationResource extends CatalogResource<DBUser> {
 	}
 	
 	@Override
-	protected Reference getSourceReference(Form form, DBUser model)
+	protected Reference getSourceReference(Form form, String model)
 			throws ResourceException {
 		return null;
 	}
-	
+	@Override
+	protected Representation post(Representation entity, Variant variant)
+			throws ResourceException {
+		throw new ResourceException(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED);
+	}
 	@Override
 	protected Representation put(Representation entity, Variant variant)
 			throws ResourceException {
@@ -89,20 +87,9 @@ public class RegistrationResource extends CatalogResource<DBUser> {
 		throw new ResourceException(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED);
 	}
 	@Override
-	protected ICallableTask createCallable(Method method, Form form, DBUser item)
+	protected ICallableTask createCallable(Method method, Form form, String item)
 			throws ResourceException {
-		Connection conn = null;
-		try {
-			String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
-			UserURIReporter reporter = new UserURIReporter(getRequest(),"");
-			DBConnection dbc = new DBConnection(getApplication().getContext(),getConfigFile());
-			conn = dbc.getConnection();
-			return new CallableUserCreator(method,item,reporter, form,getRequest().getRootRef().toString(),
-					conn,getToken(),false,usersdbname==null?"tomcat_users":usersdbname);
-		} catch (Exception x) {
-			try { conn.close(); } catch (Exception xx) {}
-			throw new ResourceException(Status.SERVER_ERROR_INTERNAL,x);
-		}
+		return null;
 	}
 	
 	public String getConfigFile() {
@@ -111,13 +98,8 @@ public class RegistrationResource extends CatalogResource<DBUser> {
 	
 	@Override
 	protected HTMLBeauty getHTMLBeauty() {
-		return new QMRF_HTMLBeauty(Resources.register);
+		return new QMRF_HTMLBeauty(Resources.notify);
 	}
 	
-	@Override
-	public IProcessor<Iterator<DBUser>, Representation> createJSONConvertor(
-			Variant variant, String filenamePrefix) throws AmbitException,
-			ResourceException {
-		return super.createJSONConvertor(variant, filenamePrefix);
-	}
+	
 }
