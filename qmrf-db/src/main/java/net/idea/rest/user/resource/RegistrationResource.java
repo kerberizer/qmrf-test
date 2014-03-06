295600feaa8beb9de6a543dd22932fd6c2a0f19d
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2014-03-06 16:11:21 +0000

    Fixes https://sourceforge.net/p/qmrf/tickets/78/

--- qmrf-db/src/main/java/net/idea/rest/user/resource/RegistrationResource.java
+++ qmrf-db/src/main/java/net/idea/rest/user/resource/RegistrationResource.java
@@ -2,6 +2,7 @@ package net.idea.rest.user.resource;
 
 import java.io.Writer;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -9,9 +10,11 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
 
+import net.idea.modbcum.i.IQueryObject;
 import net.idea.modbcum.i.exceptions.AmbitException;
 import net.idea.modbcum.i.processors.IProcessor;
 import net.idea.modbcum.i.reporter.Reporter;
+import net.idea.modbcum.p.QueryExecutor;
 import net.idea.qmrf.client.Resources;
 import net.idea.qmrf.client.Resources.Config;
 import net.idea.rest.QMRFCatalogResource;
@@ -30,6 +33,7 @@ import net.idea.restnet.i.task.ICallableTask;
 import net.idea.restnet.i.task.ITaskStorage;
 import net.idea.restnet.rdf.FactoryTaskConvertorRDF;
 import net.idea.restnet.user.DBUser;
+import net.idea.restnet.user.db.ReadUser;
 import net.idea.restnet.user.resource.UserURIReporter;
 
 import org.restlet.Context;
@@ -106,12 +110,43 @@ public class RegistrationResource extends QMRFCatalogResource<DBUser> {
 			throws ResourceException {
 		Connection conn = null;
 		try {
+						
 			String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
 			UserURIReporter reporter = new UserURIReporter(getRequest(),"");
 			DBConnection dbc = new QDBConnection(getApplication().getContext(),getDbConfig());
 			conn = dbc.getConnection();
+			
+			QueryExecutor qx = null;
+			ResultSet rs = null;
+			boolean found = false;
+			try {
+				String userName = form.getFirstValue("username");
+				DBUser user = new DBUser();
+				user.setUserName(userName);
+				ReadUser q = new ReadUser(user);
+				qx = new QueryExecutor<IQueryObject>();
+				qx.setConnection(conn);
+				qx.setCloseConnection(false);
+				rs = qx.process(q);
+				while (rs.next()) {
+					user = q.getObject(rs);
+					found = true;
+				}
+			} catch (Exception x) {
+				throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST,x);	
+			} finally {
+				try { rs.close();} catch (Exception x) {}
+				try { qx.close();} catch (Exception x) {}
+			}
+
+			if (found) throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST,"This username already exist.");
+			
 			return new QMRFCallableUserCreator(method,item,reporter, form,getRequest().getRootRef().toString(),
 					conn,getToken(),false,usersdbname==null?"tomcat_users":usersdbname);
+			
+		} catch (ResourceException x) {
+			try { conn.close(); } catch (Exception xx) {}
+			throw x;
 		} catch (Exception x) {
 			try { conn.close(); } catch (Exception xx) {}
 			throw new ResourceException(Status.SERVER_ERROR_INTERNAL,x);
