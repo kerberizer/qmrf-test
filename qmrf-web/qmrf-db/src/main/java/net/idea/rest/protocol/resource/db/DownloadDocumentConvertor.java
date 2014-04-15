d370b5f51fc47817a868b974aef772e7ace7a00d
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2014-04-15 10:16:50 +0000

    Fix to compile with JDK7

--- qmrf-web/qmrf-db/src/main/java/net/idea/rest/protocol/resource/db/DownloadDocumentConvertor.java
+++ qmrf-web/qmrf-db/src/main/java/net/idea/rest/protocol/resource/db/DownloadDocumentConvertor.java
@@ -7,12 +7,13 @@ import net.idea.modbcum.i.exceptions.AmbitException;
 import net.idea.modbcum.r.QueryReporter;
 import net.idea.rest.protocol.attachments.DBAttachment;
 import net.idea.restnet.db.convertors.AbstractObjectConvertor;
+import net.idea.restnet.db.convertors.QueryRepresentationConvertor;
 
 import org.restlet.data.MediaType;
 import org.restlet.representation.FileRepresentation;
 import org.restlet.representation.Representation;
 
-public class DownloadDocumentConvertor extends   AbstractObjectConvertor<DBAttachment, IQueryRetrieval<DBAttachment>,FileRepresentation> {
+public class DownloadDocumentConvertor extends QueryRepresentationConvertor<DBAttachment, IQueryRetrieval<DBAttachment>,FileRepresentation> {
 
 	public DownloadDocumentConvertor(
 			QueryReporter<DBAttachment, IQueryRetrieval<DBAttachment>, FileRepresentation> reporter,MediaType media,String fileNamePrefix) {
@@ -27,7 +28,7 @@ public class DownloadDocumentConvertor extends   AbstractObjectConvertor<DBAttac
 	protected File getFile(DBAttachment item) throws Exception  {
 		return new File(item.getResourceURL().toURI());
 	}
-
+/*
 	public Representation process(DBAttachment item) throws AmbitException {
 		try {
 			if (item==null) throw new AmbitException("No attachment!");
@@ -43,15 +44,21 @@ public class DownloadDocumentConvertor extends   AbstractObjectConvertor<DBAttac
 		}
 
 	}
+	*/
+	
 	
 	@Override
-	protected FileRepresentation createOutput(IQueryRetrieval<DBAttachment> query)
-			throws AmbitException {
-		return null;
-	}
-	@Override
-	public Representation process(FileRepresentation doc) throws AmbitException {
+	public Representation process(IQueryRetrieval<DBAttachment> query)
+			throws Exception {
+		//reporter.setOutput(createOutput(query));
+		Representation r =  processDoc(reporter.process(query));
+		try { reporter.close(); } catch (Exception x) {}
+		return r;
+	};	
+	
+	public Representation processDoc(FileRepresentation doc) throws AmbitException {
 		return doc;
 	}
+
 
 }
