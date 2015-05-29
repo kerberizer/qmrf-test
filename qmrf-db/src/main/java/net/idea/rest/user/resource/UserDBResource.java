f95c12a6762cb874d6af71f7f9fbf24d8576bf30
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2015-05-29 15:57:53 +0000

    throw exception if the media type is not explicitly supported, instead of rendering html

--- qmrf-db/src/main/java/net/idea/rest/user/resource/UserDBResource.java
+++ qmrf-db/src/main/java/net/idea/rest/user/resource/UserDBResource.java
@@ -50,234 +50,231 @@ import org.restlet.resource.ResourceException;
 
 /**
  * Protocol resource
+ * 
  * @author nina
- *
+ * 
  * @param <Q>
  */
-public class UserDBResource<T>	extends QMRFQueryResource<ReadUser<T>,DBUser> {
-	protected boolean readRegistrationStatus;	
-	public static final String resourceKey = "user";
-	
-	protected boolean singleItem = false;
-	protected boolean editable = true;
-	
-	public UserDBResource() {
-		super();
-		setHtmlbyTemplate(true);
-		readRegistrationStatus = true;
-	}
+public class UserDBResource<T> extends QMRFQueryResource<ReadUser<T>, DBUser> {
+    protected boolean readRegistrationStatus;
+    public static final String resourceKey = "user";
 
-	@Override
-	public boolean isHtmlbyTemplate() {
-		return headless?false:singleItem?freeMarkerSupport.isHtmlbyTemplate():false;
-	}
-	@Override
-	public String getTemplateName() {
-		return "myprofile_body.ftl";
-	}
-	
-	
-	@Override
-	protected void doInit() throws ResourceException {
-		super.doInit();
-		getVariants().add(new Variant(MediaType.TEXT_XML));
-	}
-	@Override
-	public RepresentationConvertor createConvertor(Variant variant)
-			throws AmbitException, ResourceException {
-		/*
-		if (variant.getMediaType().equals(MediaType.TEXT_PLAIN)) {
-			
-			return new StringConvertor(new PropertyValueReporter(),MediaType.TEXT_PLAIN);
-			
-		} else
-		*/ 
-		String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
-		if (usersdbname==null) usersdbname = "tomcat_users";
-		
-		String filenamePrefix = getRequest().getResourceRef().getPath();
-		if (variant.getMediaType().equals(MediaType.TEXT_URI_LIST)) {
-				return new StringConvertor(	
-						new UserURIReporter<IQueryRetrieval<DBUser>>(getRequest())
-						,MediaType.TEXT_URI_LIST,filenamePrefix);
-		} else if (variant.getMediaType().equals(MediaType.TEXT_CSV)) {
-			return new OutputWriterConvertor(
-					new UserCSVReporter<IQueryRetrieval<DBUser>>(getRequest()),
-					MediaType.TEXT_CSV);
-		} else if (variant.getMediaType().equals(MediaType.APPLICATION_JSON)) {
-			
-			return new OutputWriterConvertor(
-					new net.idea.rest.user.resource.UserJSONReporter(getRequest(),readRegistrationStatus?usersdbname:null),
-					MediaType.APPLICATION_JSON);			
-		} else if (variant.getMediaType().equals(MediaType.TEXT_XML)) {
-			return new OutputWriterConvertor(
-					new UserXMLReporter<IQueryRetrieval<DBUser>>(getRequest().getResourceRef()),
-					MediaType.TEXT_XML);			
-		} else //html
-				return new OutputWriterConvertor(
-						createHTMLReporter(headless),
-						MediaType.TEXT_HTML);
+    protected boolean singleItem = false;
+    protected boolean editable = true;
 
-	}
-	
-	@Override
-	protected QueryHTMLReporter createHTMLReporter(boolean headless)
-			throws ResourceException {
-		String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
-		if (usersdbname==null) usersdbname = "tomcat_users";
-				
-		UserHTMLReporter rep = new UserHTMLReporter(getRequest(),!singleItem,editable,(UserHTMLBeauty)getHTMLBeauty(),readRegistrationStatus?usersdbname:null);
-		rep.setHeadless(headless);
-		return rep;
-	}
+    public UserDBResource() {
+	super();
+	setHtmlbyTemplate(true);
+	readRegistrationStatus = true;
+    }
 
-	protected ReadUser getUserQuery(Object key,String search_name,Object search_value) throws ResourceException {
-		if (key==null) {
-			ReadUser query = new ReadUser();
-			if (search_value != null) {
-				if ("search".equals(search_name)) {
-					DBUser user = new DBUser();
-					String s = String.format("^%s", search_value.toString());
-					user.setLastname(s);
-					user.setFirstname(s);
-					query.setValue(user);
+    @Override
+    public boolean isHtmlbyTemplate() {
+	return headless ? false : singleItem ? freeMarkerSupport.isHtmlbyTemplate() : false;
+    }
 
-				} else if ("username".equals(search_name)) { 
-					DBUser user = new DBUser();
-					query.setCondition(EQCondition.getInstance());
-					user.setUserName(search_value.toString());
-					query.setValue(user);
-				}
-			}
-			return query;
-		}			
-		else {
-			if (key.toString().startsWith("U")) {
-				singleItem = true;
-				return new ReadUser(new Integer(Reference.decode(key.toString().substring(1))));
-			} else throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
-		}		
+    @Override
+    public String getTemplateName() {
+	return "myprofile_body.ftl";
+    }
+
+    @Override
+    protected void doInit() throws ResourceException {
+	super.doInit();
+	getVariants().add(new Variant(MediaType.TEXT_XML));
+    }
+
+    @Override
+    public RepresentationConvertor createConvertor(Variant variant) throws AmbitException, ResourceException {
+	/*
+	 * if (variant.getMediaType().equals(MediaType.TEXT_PLAIN)) {
+	 * 
+	 * return new StringConvertor(new
+	 * PropertyValueReporter(),MediaType.TEXT_PLAIN);
+	 * 
+	 * } else
+	 */
+	String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
+	if (usersdbname == null)
+	    usersdbname = "tomcat_users";
+
+	String filenamePrefix = getRequest().getResourceRef().getPath();
+	if (variant.getMediaType().equals(MediaType.TEXT_URI_LIST)) {
+	    return new StringConvertor(new UserURIReporter<IQueryRetrieval<DBUser>>(getRequest()),
+		    MediaType.TEXT_URI_LIST, filenamePrefix);
+	} else if (variant.getMediaType().equals(MediaType.TEXT_CSV)) {
+	    return new OutputWriterConvertor(new UserCSVReporter<IQueryRetrieval<DBUser>>(getRequest()),
+		    MediaType.TEXT_CSV);
+	} else if (variant.getMediaType().equals(MediaType.APPLICATION_JSON)) {
+
+	    return new OutputWriterConvertor(new net.idea.rest.user.resource.UserJSONReporter(getRequest(),
+		    readRegistrationStatus ? usersdbname : null), MediaType.APPLICATION_JSON);
+	} else if (variant.getMediaType().equals(MediaType.TEXT_XML)) {
+	    return new OutputWriterConvertor(
+		    new UserXMLReporter<IQueryRetrieval<DBUser>>(getRequest().getResourceRef()), MediaType.TEXT_XML);
+	} else if (variant.getMediaType().equals(MediaType.TEXT_HTML)) {
+	    return new OutputWriterConvertor(createHTMLReporter(headless), MediaType.TEXT_HTML);
 	}
-	@Override
-	protected ReadUser createQuery(Context context, Request request, Response response)
-			throws ResourceException {
-		Form form = request.getResourceRef().getQueryAsForm();
-		String search_name = null;
-		Object search_value = null;
-		try {
-			search_name = "search";
-			search_value = form.getFirstValue(search_name);
-		} catch (Exception x) {
-			search_value = null;
-		}		
-		if (search_value==null)
-		try {
-			search_name = "username";
-			search_value = form.getFirstValue(search_name);
-		} catch (Exception x) {
-			search_value = null;
-		}				
-		try {
-			String n = form.getFirstValue("new");
-			editable = n==null?false:Boolean.parseBoolean(n);
-		} catch (Exception x) {
-			editable = false;
+	else
+	    throw new ResourceException(Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE);
+
+    }
+
+    @Override
+    protected QueryHTMLReporter createHTMLReporter(boolean headless) throws ResourceException {
+	String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
+	if (usersdbname == null)
+	    usersdbname = "tomcat_users";
+
+	UserHTMLReporter rep = new UserHTMLReporter(getRequest(), !singleItem, editable,
+		(UserHTMLBeauty) getHTMLBeauty(), readRegistrationStatus ? usersdbname : null);
+	rep.setHeadless(headless);
+	return rep;
+    }
+
+    protected ReadUser getUserQuery(Object key, String search_name, Object search_value) throws ResourceException {
+	if (key == null) {
+	    ReadUser query = new ReadUser();
+	    if (search_value != null) {
+		if ("search".equals(search_name)) {
+		    DBUser user = new DBUser();
+		    String s = String.format("^%s", search_value.toString());
+		    user.setLastname(s);
+		    user.setFirstname(s);
+		    query.setValue(user);
+
+		} else if ("username".equals(search_name)) {
+		    DBUser user = new DBUser();
+		    query.setCondition(EQCondition.getInstance());
+		    user.setUserName(search_value.toString());
+		    query.setValue(user);
 		}
-		Object key = request.getAttributes().get(UserDBResource.resourceKey);		
-		try {
-			return getUserQuery(key,search_name,search_value);
-		}catch (ResourceException x) {
-			throw x;
-		} catch (Exception x) {
-			throw new ResourceException(
-					Status.CLIENT_ERROR_BAD_REQUEST,
-					String.format("Invalid protocol id %d",key),
-					x
-					);
-		}
-	} 
+	    }
+	    return query;
+	} else {
+	    if (key.toString().startsWith("U")) {
+		singleItem = true;
+		return new ReadUser(new Integer(Reference.decode(key.toString().substring(1))));
+	    } else
+		throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+	}
+    }
 
-	@Override
-	protected QueryURIReporter<DBUser, ReadUser<T>> getURUReporter(
-			Request baseReference) throws ResourceException {
-		return new UserURIReporter(getRequest());
+    @Override
+    protected ReadUser createQuery(Context context, Request request, Response response) throws ResourceException {
+	Form form = request.getResourceRef().getQueryAsForm();
+	String search_name = null;
+	Object search_value = null;
+	try {
+	    search_name = "search";
+	    search_value = form.getFirstValue(search_name);
+	} catch (Exception x) {
+	    search_value = null;
 	}
-	
-	@Override
-	protected TaskCreator getTaskCreator(Form form, Method method,
-			boolean async, Reference reference) throws Exception {
-		if (Method.POST.equals(method) && (form.getFirstValue(ReadUser.fields.email.name())==null)) {
-			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST,"e-mail address not specified!");
-		}
-		return super.getTaskCreator(form, method, async, reference);
+	if (search_value == null)
+	    try {
+		search_name = "username";
+		search_value = form.getFirstValue(search_name);
+	    } catch (Exception x) {
+		search_value = null;
+	    }
+	try {
+	    String n = form.getFirstValue("new");
+	    editable = n == null ? false : Boolean.parseBoolean(n);
+	} catch (Exception x) {
+	    editable = false;
 	}
-	
-	@Override
-	protected CallableProtectedTask<String> createCallable(Method method,
-			Form form, DBUser item) throws ResourceException {
+	Object key = request.getAttributes().get(UserDBResource.resourceKey);
+	try {
+	    return getUserQuery(key, search_name, search_value);
+	} catch (ResourceException x) {
+	    throw x;
+	} catch (Exception x) {
+	    throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, String.format("Invalid protocol id %d", key),
+		    x);
+	}
+    }
 
-		
-		Connection conn = null;
-		try {
-			String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
-			UserURIReporter reporter = new UserURIReporter(getRequest(),"");
-			DBConnection dbc = new QDBConnection(getApplication().getContext(),getDbConfig());
-			conn = dbc.getConnection();
-			return new QMRFCallableUserCreator(method,item,reporter, form,getRequest().getRootRef().toString(),
-					conn,getToken(),false,usersdbname==null?"tomcat_users":usersdbname);
-		} catch (Exception x) {
-			try { conn.close(); } catch (Exception xx) {}
-			throw new ResourceException(Status.SERVER_ERROR_INTERNAL,x);
-		}
-	}
+    @Override
+    protected QueryURIReporter<DBUser, ReadUser<T>> getURUReporter(Request baseReference) throws ResourceException {
+	return new UserURIReporter(getRequest());
+    }
 
-	protected String getObjectURI(Form queryForm) throws ResourceException {
-		return null;		
+    @Override
+    protected TaskCreator getTaskCreator(Form form, Method method, boolean async, Reference reference) throws Exception {
+	if (Method.POST.equals(method) && (form.getFirstValue(ReadUser.fields.email.name()) == null)) {
+	    throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "e-mail address not specified!");
 	}
-	
-	@Override
-	protected ReadUser<T> createUpdateQuery(Method method, Context context,
-			Request request, Response response) throws ResourceException {
-		Object key = request.getAttributes().get(UserDBResource.resourceKey);
-		if (Method.POST.equals(method)) {
-			if (key==null) return null;//post allowed only on /user level, not on /user/id
-		} else {
-			if (key!=null) return super.createUpdateQuery(method, context, request, response);
-		}
-		throw new ResourceException(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED);
+	return super.getTaskCreator(form, method, async, reference);
+    }
+
+    @Override
+    protected CallableProtectedTask<String> createCallable(Method method, Form form, DBUser item)
+	    throws ResourceException {
+
+	Connection conn = null;
+	try {
+	    String usersdbname = getContext().getParameters().getFirstValue(Config.users_dbname.name());
+	    UserURIReporter reporter = new UserURIReporter(getRequest(), "");
+	    DBConnection dbc = new QDBConnection(getApplication().getContext(), getDbConfig());
+	    conn = dbc.getConnection();
+	    return new QMRFCallableUserCreator(method, item, reporter, form, getRequest().getRootRef().toString(),
+		    conn, getToken(), false, usersdbname == null ? "tomcat_users" : usersdbname);
+	} catch (Exception x) {
+	    try {
+		conn.close();
+	    } catch (Exception xx) {
+	    }
+	    throw new ResourceException(Status.SERVER_ERROR_INTERNAL, x);
 	}
-	@Override
-	protected FactoryTaskConvertor getFactoryTaskConvertor(ITaskStorage storage)
-			throws ResourceException {
-		return new FactoryTaskConvertorRDF<Object>(storage,getHTMLBeauty()) {
-			@Override
-			public synchronized Reporter<Iterator<UUID>, Writer> createTaskReporterHTML(
-					Request request,ResourceDoc doc,HTMLBeauty htmlbeauty) throws AmbitException, ResourceException {
-				return	new UserTaskHTMLReporter(storage,request,doc,htmlbeauty);
-			}			
-		};
+    }
+
+    protected String getObjectURI(Form queryForm) throws ResourceException {
+	return null;
+    }
+
+    @Override
+    protected ReadUser<T> createUpdateQuery(Method method, Context context, Request request, Response response)
+	    throws ResourceException {
+	Object key = request.getAttributes().get(UserDBResource.resourceKey);
+	if (Method.POST.equals(method)) {
+	    if (key == null)
+		return null;// post allowed only on /user level, not on /user/id
+	} else {
+	    if (key != null)
+		return super.createUpdateQuery(method, context, request, response);
 	}
-	
-	
-	@Override
-	protected boolean isAllowedMediaType(MediaType mediaType)
-			throws ResourceException {
-		return MediaType.APPLICATION_WWW_FORM.equals(mediaType);
-	}
-	
-	@Override
-	protected HTMLBeauty getHTMLBeauty() {
-		if (htmlBeauty==null) htmlBeauty =  new UserHTMLBeauty(Resources.user);
-		return htmlBeauty;
-	}
-	
-	@Override
-	protected Map<String, Object> getMap(Variant variant)
-			throws ResourceException {
-		Map<String, Object> map = super.getMap(variant);
-		map.put("myprofile", false);
-		return map;
-	}
-	
-	
+	throw new ResourceException(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    protected FactoryTaskConvertor getFactoryTaskConvertor(ITaskStorage storage) throws ResourceException {
+	return new FactoryTaskConvertorRDF<Object>(storage, getHTMLBeauty()) {
+	    @Override
+	    public synchronized Reporter<Iterator<UUID>, Writer> createTaskReporterHTML(Request request,
+		    ResourceDoc doc, HTMLBeauty htmlbeauty) throws AmbitException, ResourceException {
+		return new UserTaskHTMLReporter(storage, request, doc, htmlbeauty);
+	    }
+	};
+    }
+
+    @Override
+    protected boolean isAllowedMediaType(MediaType mediaType) throws ResourceException {
+	return MediaType.APPLICATION_WWW_FORM.equals(mediaType);
+    }
+
+    @Override
+    protected HTMLBeauty getHTMLBeauty() {
+	if (htmlBeauty == null)
+	    htmlBeauty = new UserHTMLBeauty(Resources.user);
+	return htmlBeauty;
+    }
+
+    @Override
+    protected Map<String, Object> getMap(Variant variant) throws ResourceException {
+	Map<String, Object> map = super.getMap(variant);
+	map.put("myprofile", false);
+	return map;
+    }
+
 }
