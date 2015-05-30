97e3bd8328e8ffaa8ee4b71703bad651686cf842
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2015-05-30 07:17:43 +0000

    as it says ... ParanoidDirectory barely to set some headers of the standard Restlet Directory class

---
+++ qmrf-war/src/main/java/net/idea/qmrf/rest/ParanoidDirectory.java
@@ -0,0 +1,30 @@
+package net.idea.qmrf.rest;
+
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.CacheDirective;
+import org.restlet.data.Form;
+import org.restlet.data.ServerInfo;
+import org.restlet.resource.Directory;
+
+public class ParanoidDirectory extends Directory {
+    public ParanoidDirectory(Context context, String uri) {
+	super(context, uri);
+    }
+
+    @Override
+    public void handle(Request request, Response response) {
+	Form headers = (Form) response.getAttributes().get("org.restlet.http.headers");
+	if (headers == null) {
+	    headers = new Form();
+	    response.getAttributes().put("org.restlet.http.headers", headers);
+	}
+	headers.removeAll("X-Frame-Options");
+	headers.add("X-Frame-Options", "SAMEORIGIN");
+	response.getCacheDirectives().add(CacheDirective.noCache());
+	ServerInfo si = response.getServerInfo();
+	si.setAgent("Restlet");
+	super.handle(request, response);
+    }
+}
