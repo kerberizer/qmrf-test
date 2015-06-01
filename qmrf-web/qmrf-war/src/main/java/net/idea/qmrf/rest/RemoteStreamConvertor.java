ea323d1f7cf8d1285b6bb8432138b41f48ef2408
Nina G. JELIAZKOVA <jeliazkova.nina@gmail.com>
2015-06-01 19:35:04 +0000

    stream copy

--- qmrf-web/qmrf-war/src/main/java/net/idea/qmrf/rest/RemoteStreamConvertor.java
+++ qmrf-web/qmrf-war/src/main/java/net/idea/qmrf/rest/RemoteStreamConvertor.java
@@ -17,58 +17,68 @@ import org.restlet.representation.OutputRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ResourceException;
 
-
-public class RemoteStreamConvertor extends DefaultAmbitProcessor<URL,Representation> {
-	private String root = "http://localhost";
-	private MediaType media = MediaType.APPLICATION_JSON ;
-	/**
+public class RemoteStreamConvertor extends DefaultAmbitProcessor<URL, Representation> {
+    private String root = "http://localhost";
+    private MediaType media = MediaType.APPLICATION_JSON;
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 5395806328786966562L;
+    private static final long serialVersionUID = 5395806328786966562L;
 
-	public RemoteStreamConvertor(String root,MediaType media) {
-		super();
-		this.root = root;
-		this.media = media;
-	}
-	@Override
-	public Representation process(final URL url) throws Exception {
-		//we don't want to proxy everything ;)
-		if (!url.toExternalForm().startsWith(root)) throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
-		OutputRepresentation rep = new OutputRepresentation(media) {
-            @Override
-            public void write(OutputStream stream) throws IOException {
-            	InputStream in = null;
-            	try {
-            		URLConnection conn = url.openConnection();
-            		if (conn instanceof HttpURLConnection) {
-            			HttpURLConnection cnx = (HttpURLConnection)conn;
-            			cnx.setAllowUserInteraction(false);         
-            			cnx.setDoOutput(true);
-            			cnx.addRequestProperty("Referer", root);
-            		}
-                	in = conn.getInputStream();
-               		DownloadTool.download(in, stream);
-            		//writer.flush();
-            		//stream.flush();
-            	} catch (Exception x) {
-            		Throwable ex = x;
-            		while (ex!=null) {
-            			if (ex instanceof IOException) 
-            				throw (IOException)ex;
-            			ex = ex.getCause();
-            		}
-            		Context.getCurrentLogger().warning(x.getMessage()==null?x.toString():x.getMessage());
+    public RemoteStreamConvertor(String root, MediaType media) {
+	super();
+	this.root = root;
+	this.media = media;
+    }
 
-            	} finally {
+    @Override
+    public Representation process(final URL url) throws Exception {
+	// we don't want to proxy everything ;)
+	if (!url.toExternalForm().startsWith(root))
+	    throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+	OutputRepresentation rep = new OutputRepresentation(media) {
+	    @Override
+	    public void write(OutputStream stream) throws IOException {
+		InputStream in = null;
+		try {
+		    URLConnection conn = url.openConnection();
+		    if (conn instanceof HttpURLConnection) {
+			HttpURLConnection cnx = (HttpURLConnection) conn;
+			cnx.setAllowUserInteraction(false);
+			cnx.setDoOutput(true);
+			cnx.addRequestProperty("Referer", root);
+		    }
+		    in = conn.getInputStream();
+		    DownloadTool.download(in, stream);
+		    // writer.flush();
+		    // stream.flush();
+		} catch (Exception x) {
+		    Throwable ex = x;
+		    while (ex != null) {
+			if (ex instanceof IOException)
+			    throw (IOException) ex;
+			ex = ex.getCause();
+		    }
+		    Context.getCurrentLogger().warning(x.getMessage() == null ? x.toString() : x.getMessage());
 
-            		try {if (stream !=null) stream.flush(); } catch (Exception x) { x.printStackTrace();}
-            		try {if (in !=null) in.close(); } catch (Exception x) { x.printStackTrace();}
-            	}
-            }
-        };	
-        //setDisposition(rep);
-        return rep;
-	}
+		} finally {
+
+		    try {
+			if (stream != null)
+			    stream.flush();
+		    } catch (Exception x) {
+		    }
+		    try {
+			if (in != null)
+			    in.close();
+		    } catch (Exception x) {
+
+		    }
+		}
+	    }
+	};
+	// setDisposition(rep);
+	return rep;
+    }
 
 }
